cd331995a90e1f175cfde20cefe4c8546d891ae3
Bassma akhiri <basma.akhiri@centrale-casablanca.ma>
2022-11-05 02:34:19 +0100

    go to test

---
+++ vessel.py
@@ -0,0 +1,52 @@
+class vessels:
+    def __init__(self, coordinates, max_hits, weapon):
+        self.coordinates=coordinates
+        self.max_hits=max_hits
+        self.weapon=weapon
+
+    def __getcoordinates(self):
+        return self.coordinates
+
+    def __getmax_hits(self):
+        return self.max_hits
+
+    def __getweapon(self):
+        return self.weapon
+
+    def __go_to__(x,y,z):
+        return x,y,z
+    pass
+    def __get_coordinates():
+
+
+    def __go_to__
+        (x,y,z):
+        x_d = self.x
+        y_d = self.y
+        z_d = self.z
+        if direction == "y":
+            y_d = self.y + move
+        elif direction == "-y:
+            y_d = self.y - move
+        elif direction == "z":
+            z_d = self.z + move
+        elif direction == "-z":
+            z_d = self.z - move
+        elif direction == "x":
+            x_d = self.x + move
+        elif direction == "-x":
+            x_d = self.x - move
+        import unittest
+
+        class Vessels(unittest.TestCase):
+            def go_to(self):
+                self.assertEqual(True, False)
+
+        if __name__ == '__main__':
+            unittest.main()
+
+        self.x = x_d
+        self.y = y_d
+        self.z = z_d
+        return 0
+

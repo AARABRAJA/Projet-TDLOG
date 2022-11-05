44968aaa77c6f3dc7a70384106fd5b12b8ae7636
ASSMINA <fatimazahra.assmina@centrale-casablanca.ma>
2022-11-05 09:43:08 +0100

    Ajout de la deuxième fonction test

--- go_to.py
+++ go_to.py
@@ -1,6 +1,7 @@
 import unittest
 from Vessels import vessel
 #Vessel nom du fichier, vessel nom de la classe
+
 class vessel(unittest.TestCase):
     def test_go_to(self,x,y,z):
         self.assertEqual(vessel.getcoordinates(self,x,y,z), (x,y,z))
